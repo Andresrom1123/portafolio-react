c66a4526ddd5ecafb6b0200dfe120f24aa4942ae
Andresrom1123 <andres2016_romero@hotmail.com>
2022-05-11 13:58:23 -0500

    created banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { social } from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="banner p-5 d-flex justify-content-center justify-content-md-start align-items-center">
+        <div>
+          <div className="d-flex justify-content-center justify-content-md-start align-items-center">
+            <div>
+              <div className="underline"></div>
+              <h1>I'm Andres</h1>
+              <h4 className="text-white">Web Developer</h4>
+              <div className="btn-group-lg mt-4">
+                <button className="btn btn-outline-warning">Contact me</button>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex mt-4">
+            {social.map((socialIcon) => {
+              const { id, url, icon } = socialIcon;
+              return (
+                <a
+                  key={id}
+                  href={url}
+                  className="me-4 fs-5  bg-black px-2 text-white rounded"
+                >
+                  {icon}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaLinkedinIn, FaHackerrank, FaGithubSquare } from "react-icons/fa";
+
+export const social = [
+  {
+    id: 1,
+    url: "",
+    icon: <FaLinkedinIn />,
+  },
+  {
+    id: 2,
+    url: "",
+    icon: <FaHackerrank />,
+  },
+  {
+    id: 3,
+    url: "",
+    icon: <FaGithubSquare />,
+  },
+];

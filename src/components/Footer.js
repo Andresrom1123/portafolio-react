c762d9d036cff66cf0b06a7cd35f6ca2a5135ff5
Andresrom1123 <andres2016_romero@hotmail.com>
2022-07-04 12:49:13 -0500

    add weather app practice

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,9 @@ import Icons from "./Icons";
 
 const Footer = () => {
   return (
-    <footer className="p-5 bg-black d-flex justify-content-center footer">
+    <footer className="p-5 bg-black d-flex align-items-center flex-column footer">
       <Icons classname="text-white me-2 fs-2" />
+      <p className="text-white mt-3">2022 - Andres Romero</p>
     </footer>
   );
 };
--- src/data/experience&projects.js
+++ src/data/experience&projects.js
@@ -18,4 +18,14 @@ export const projects = [
     url: "https://andres-romero-crypto.netlify.app/",
     stack: ["React", "Firebase"],
   },
+  {
+    id: 2,
+    description:
+      "Application where you can check the climate of Mexico in all its cities",
+    github: "https://github.com/Andresrom1123/weather-app-react",
+    title: "weather app",
+    image: "project-weather.avif",
+    url: "https://andres-romero-weather-app.netlify.app/",
+    stack: ["React"],
+  },
 ];
